d7a9f782b39daa9fe9fd0d907f087316c4e9cdb0
Mayra Landa <mlandae16@gmail.com>
2024-10-08 16:17:22 -0700

    add sql to index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 
 app.get("/", (req, res) => {
   res.send("Hello World");
@@ -8,3 +9,24 @@ app.get("/", (req, res) => {
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
 });
+
+
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+pool.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database', err);
+  } else {
+    console.log('Connected to the PostgreSQL database');
+  }
+});
+
+module.exports = pool;
