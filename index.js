68fcce417155af7eb8eb9aec75842f212cc45338
Mayra Landa <mlandae16@gmail.com>
2024-10-21 16:55:13 -0700

    add views folder

--- index.js
+++ index.js
@@ -2,8 +2,35 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.get("/", async (req, res) => {
+  try{
+    const result = await pool.query("SELECT * FROM songs");
+    res.render("index", {songs: result.rows});
+  } catch (err){
+    console.error("Error fetching songs:", err);
+    res.status(500).send("Error fetching songs");å
+  }
+});
+
+app.get("/songs/new", (req, res) => {
+  res.render("add-song");
+});
+
+app.post("/songs", async (req, res) => {
+  const {title, artist, album, genre, quantity} = req.body;
+
+  const query = `
+  INSERT INTO songs (title, artist, album, quantity)
+  VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *`;
+  const values = [title, artist, album, quantity];
+
+  try{
+    await pool.query(query, values);
+    res.redirect("/");
+  } catch (err){
+    console.error("Error adding song:", err);
+    res.status(500).send("Error adding song");
+  }
 });
 
 app.listen(3000, () => {
